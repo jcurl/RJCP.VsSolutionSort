e33cbabb35442af87039add92bce6438850a8066
Jason Curl <jcurl@arcor.de>
2024-01-15 21:38:36 +0100

    SolutionSort: Build for Net 6 and 8
    Update the rollforward policy to `Major`, that will allow the binary to run on systems that don't have the exact major version of the runtime installed.
    
    Issue: DNX-15

--- SolutionSort/SolutionSort/SolutionFormatException.cs
+++ SolutionSort/SolutionSort/SolutionFormatException.cs
@@ -6,7 +6,6 @@
     /// <summary>
     /// There is a format error in the solution file that it can't be parsed.
     /// </summary>
-    [Serializable]
     public class SolutionFormatException : Exception
     {
         /// <summary>
@@ -19,13 +18,5 @@
         /// </summary>
         /// <param name="message">The message that describes the error.</param>
         public SolutionFormatException(string message) : base(message) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="SolutionFormatException"/> class.
-        /// </summary>
-        /// <param name="info">The <see cref="SerializationInfo" /> that holds the serialized object data about the exception being thrown.</param>
-        /// <param name="context">The <see cref="StreamingContext" /> that contains contextual information about the source or destination.</param>
-        protected SolutionFormatException(SerializationInfo info, StreamingContext context)
-            : base(info, context) { }
     }
 }
